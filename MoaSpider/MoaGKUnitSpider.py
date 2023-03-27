273e2c8d88807c4d9d33ba713caeb8b3067de555
Rain <lgy.1992.cn@gmail.com>
2023-03-27 21:49:07 +0800

    增加公开栏目单元解析

---
+++ MoaSpider/MoaGKUnitSpider.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+from base import SpiderBase
+from .MoaReportPageSpider import MoaReportPageSpider
+
+class MoaGkUnitSpider(SpiderBase):
+    """爬取分子单元块的页面"""
+    def __init__(self,category,page_url):
+        super().__init__()
+        self.category=category
+        self.page_url=page_url
+
+
+    def request(self):
+        resp=self.client.get(self.page_url)
+        resp.encoding="utf8"
+        return resp.text
+
+    def parser(self,html):
+        doc=BeautifulSoup(html,features="html.parser")
+        unit_list=doc.find_all(class_="gknrtitle")
+        print(len(unit_list))
+        for unit in unit_list:
+            unit_title=unit.span.string
+            href=unit.a['href']
+            page_url=urljoin(self.page_url,href)
+            MoaReportPageSpider(page_url)
+            stop=MoaReportPageSpider.run()
+            
+
+        
+
+    def storage(self):
+        pass
+
+
+    def run(self):
+        data,status=self.request()
+        data,status=self.parser(data)
