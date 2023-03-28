2406973692aece4ab779d384badee79e99b73e6a
Rain <lgy.1992.cn@gmail.com>
2023-03-28 21:10:18 +0800

    完善SpiderBase类，增加run方法，更新请求方法

--- base.py
+++ base.py
@@ -2,10 +2,50 @@ import requests
 import exception
 import functools
 
+import logging
+
+
+
 class SpiderBase():
-    def __init__(self):
-        self.client=requests.Session()
+    def __init__(self,url,client: requests.Session = None):
+        self.url=url
+        self.client=client or requests.Session()
         self.client.headers["User-Agent"]="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+
+    def run(self):
+        try:
+            raw_data=self.request()
+            parsed_data=self.parse(raw_data)
+            self.storage(parsed_data)
+        except NotImplementedError:
+            pass
+        except exception.SpiderCreateError as e:
+            logging.error("创建任务失败, 类型:{}, 地址:{}".format(self.__class__.__name__,self.url),
+                          exc_info=True)
+        except exception.SpiderRequestError as e:
+            logging.error("请求失败, 类型:{}, 地址:{}".format(self.__class__.__name__,self.url),
+                          exc_info=True)
+        except exception.SpiderParseError as e:
+            logging.error("解析失败, 类型:{}, 地址:{}".format(self.__class__.__name__,self.url),
+                          exc_info=True)
+        except exception.SpiderStorageError as e:
+            logging.error("存储失败, 类型:{}, 地址:{}".format(self.__class__.__name__,self.url),
+                          exc_info=True)
+        except exception.SpiderTaskEndError as e:
+            logging.info("子任务创建结束, 类型:{}, 地址:{}".format(self.__class__.__name__,self.url))
+            raise
+        
+        logging.info("爬取完成, 类型:{}, 地址:{}".format(self.__class__.__name__,self.url))
+
+
+    def request(self):
+        raise NotImplementedError
+    
+    def parse(self):
+        raise NotImplementedError
+
+    def storage(self):
+        raise NotImplementedError
 
     @functools.wraps(requests.Session.request)
     def do_request(self,*args,**kwargs):
@@ -17,15 +57,30 @@ class SpiderBase():
         return resp
     
 
-    get=functools.wraps(requests.Session.get)(functools.partial(do_request,method='get'))
-    # def get(self,*args,**kwargs):
-    #     return self.do_request('get',*args,**kwargs)
+    @functools.wraps(requests.Session.get)
+    def get(self,*args,**kwargs):
+        return self.do_request('get',*args,**kwargs)
 
-    put=functools.wraps(requests.Session.put,functools.partial(do_request,'put'))
-    post=functools.wraps(requests.Session.post,functools.partial(do_request,'post'))
-    delete=functools.wraps(requests.Session.delete,functools.partial(do_request,'delete'))
-    head=functools.wraps(requests.Session.head,functools.partial(do_request,'head'))
-    options=functools.wraps(requests.Session.options,functools.partial(do_request,'options'))
+    @functools.wraps(requests.Session.get)
+    def put(self,*args,**kwargs):
+        return self.do_request('put',*args,**kwargs)
+    
+    @functools.wraps(requests.Session.get)
+    def post(self,*args,**kwargs):
+        return self.do_request('post',*args,**kwargs)
+    
+    @functools.wraps(requests.Session.get)
+    def delete(self,*args,**kwargs):
+        return self.do_request('delete',*args,**kwargs)
+    
+    @functools.wraps(requests.Session.get)
+    def head(self,*args,**kwargs):
+        return self.do_request('head',*args,**kwargs)
+    
+    @functools.wraps(requests.Session.get)
+    def options(self,*args,**kwargs):
+        return self.do_request('options',*args,**kwargs)
+
 
 
     
