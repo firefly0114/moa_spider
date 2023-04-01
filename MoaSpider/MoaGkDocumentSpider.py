92265ae52ce3ec4074239f5d00a17ade46244eb9
Rain <lgy.1992.cn@gmail.com>
2023-04-01 21:47:17 +0800

    允许来源为空

--- MoaSpider/MoaGkDocumentSpider.py
+++ MoaSpider/MoaGkDocumentSpider.py
@@ -7,10 +7,6 @@ from base import SpiderBase
 class MoaGkDocumentSpider(SpiderBase):
     """爬取指定的文章"""
 
-    def __init__(self,category,url):
-        super().__init__(url)
-        self.category=category
-
     def request(self):
         resp=self.get(self.url)
         resp.encoding="utf8"
@@ -20,17 +16,21 @@ class MoaGkDocumentSpider(SpiderBase):
         doc=BeautifulSoup(html,features="html.parser")
         content_elm=doc.find(class_="gsj_content")
         title=content_elm.find(class_="xxgk_title").string
-        subtitle_elm=content_elm.find(class_="subtitle").string
+        subtitle_elm=content_elm.find(class_="subtitle")
         pubtime=subtitle_elm.find(class_="pubtime").string.lstrip("发布时间：")
-        source=subtitle_elm.find(class_="pubtime source").string.lstrip("来源：")
-        text=doc.find(class_="gsj_htmlcon_bot").string
 
         result={
             "category":self.category,
             "title":title,
             "pubtime":pubtime,
-            "source":source,
-            "text":text,
         }
+
+        source_elm=subtitle_elm.find(class_="pubtime source")
+        if source_elm:
+            result["source"]=source_elm.string.lstrip("来源：")
+
+        result["text"]=doc.find(class_="gsj_htmlcon_bot").string
+
+
         return result
         
